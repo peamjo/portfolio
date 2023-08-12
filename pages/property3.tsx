0933be2f400e4ec9335c1f99654b2675002fccc3
peamjo <peam@live.com>
2023-08-12 20:53:29 +0700

    final 2

--- pages/property3.tsx
+++ pages/property3.tsx
@@ -8,7 +8,7 @@ const Property3 = () => {
         title="Using Terraform to build a Dev Environment"
         tech_all="Terraform"
         overview="For this project I wanted to learn about IaC (Infrastructure as Code) so I built a Dev Environment using Terraform.
-It was the first time using any IaaS platform and it allowed me to understand how it worked a lot more. By not accessing the actual AWS and being able to interact with a lot of the services using code instead, I understood a lot more about how IaaS works and enjoyed working with it."
+It was the first time using any IaC program and it allowed me to understand how it worked a lot more. By not accessing the actual AWS and being able to interact with a lot of the services using code instead, I understood a lot more about how IaC works and enjoyed working with it."
         background={project3Img}
         demo="/assets/terraform.png"
         tech_list={["Terraform", "IAM", "VPC", "EC2"]}
