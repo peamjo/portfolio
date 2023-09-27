2bbff199b59ab9b0878863c9d4a86ac4b81eb460
peamjo <peam@live.com>
2023-09-27 17:17:26 +0700

    edit #3

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <div id="about" className="w-full p-2 flex items-center py-16">
+    <div id="about" className="flex items-center w-full p-2 py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="uppercase text-xl tracking-widest text-[#5651e5]">
@@ -36,14 +36,14 @@ const About = () => {
             </b>{" "}
             certificate alongside the{" "}
             <b className="text-[#5651e5]">AWS Certified Cloud Practitioner</b>{" "}
-            certicate.
+            certicate. I am currently working towards the AWS Certified Cloud Associate certificate.
           </p>
           <p className="py-2 text-gray-600">
-            I aim to start working in the Cloud industry as a
-            Solutions Architect, to not only gain experience in different
-            Clouds, but also develop my skills in communications with business
-            clients and customers who want to transition into or improve their
-            usage of the Cloud.
+            I aim to start working in the Cloud industry as a Solutions
+            Architect, to not only gain experience in different Clouds, but also
+            develop my skills in communications with business clients and
+            customers who want to transition into or improve their usage of the
+            Cloud.
           </p>
         </div>
         <div className="flex items-center justify-center w-full h-auto p-4 m-auto duration-300 ease-in shadow-cl shadow-gray-400 rounded-xl hover:scale-105">
