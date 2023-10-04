2110d06ef2e29aaf2d3eb35805941b1f4fa98920
peamjo <peam@live.com>
2023-10-05 01:34:35 +0700

    renaming #2

--- pages/projects/jenkins.tsx
+++ pages/projects/jenkins.tsx
@@ -5,9 +5,9 @@ const Jenkins = () => {
   return (
     <div>
       <Property
-        title="Jenkins Demo"
-        tech_all="Jenkins"
-        overview= "In this project, I want to try creating a CI/CD pipeline to create a stack in CloudFormation using an automation server. I chose to use Jenkins in this project.
+        title="Event Driven Architecture"
+        tech_all="Microservices"
+        overview="In this project, I want to try creating a CI/CD pipeline to create a stack in CloudFormation using an automation server. I chose to use Jenkins in this project.
     1. Install Jenkins from AWS Marketplace
         a. First I went on the AWS marketplace to look for the Jenkins package. This particular one is run on Debian. 
         b. I then created the Jenkins EC2 instance.
@@ -32,7 +32,7 @@ const Jenkins = () => {
     Actually using Jenkins as part of a CI/CD pipeline to make sure that the infrastructure is always ready to be built made it so much easier for me to understand its concept.
     I ran across some unexpected problems and realized that these little things are sometimes inevitable and the more I know and understand the technology to a deeper level, the faster I can be to solve these problems"
         background={project4Img}
-        demo="/assets/projects/project4.png"
+        demo="/assets/projects/project6.png"
         tech_list={["Jenkins", "EC2", "CloudFormation"]}
       />
     </div>
