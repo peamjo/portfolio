846f77d35ad886b89c3b2427e9d865005d99c1b5
peamjo <peam@live.com>
2023-10-04 17:59:19 +0700

    project 5 update

--- pages/property5.tsx
+++ pages/property5.tsx
@@ -5,13 +5,13 @@ const Property5 = () => {
   return (
     <div>
       <Property
-        title="Using Terraform to build a Dev Environment"
-        tech_all="Terraform"
+        title="LevelUp! Lab for Serverless"
+        tech_all="Serverless"
         overview="For this project I wanted to learn about IaC (Infrastructure as Code) so I built a Dev Environment using Terraform.
 It was the first time using any IaC program and it allowed me to understand how it worked a lot more. By not accessing the actual AWS and being able to interact with a lot of the services using code instead, I understood a lot more about how IaC works and enjoyed working with it."
         background={project3Img}
         demo="/assets/terraform.png"
-        tech_list={["Terraform", "IAM", "VPC", "EC2"]}
+        tech_list={["API Gateway", "AWS Lambda", "Amazon DynamoDB"]}
       />
     </div>
   );
