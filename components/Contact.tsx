07288f9bed22450840183865fcbde888e5fa833a
peamjo <peam@live.com>
2023-11-16 02:10:32 +0700

    update 3.1

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -31,7 +31,7 @@ const Contact = () => {
               </div>
               <div>
                 <h2 className="py-2 mt-4">Peam Jo</h2>
-                <p>Cybersecurity/Data Engineer</p>
+                <p>Data Analyst</p>
                 <p className="py-4">I am available for full-time positions.</p>
               </div>
               <div>
